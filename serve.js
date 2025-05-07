ca903136b3f78505538583e43e193c93bbd134ab
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-05-07 08:59:54 -0300

    Create serve.js

---
+++ serve.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Troque 'COM3' pela porta do seu HC-05 (ex: COM4, /dev/ttyUSB0, etc.)
+const port = new SerialPort({ path: 'COM3', baudRate: 9600 });
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+app.use(express.static('public'));
+
+parser.on('data', data => {
+  console.log('pH recebido:', data);
+  io.emit('arduinoData', data);  // Envia para o navegador
+});
+
+server.listen(3000, () => {
+  console.log('Acesse http://localhost:3000 para ver os dados.');
+});
